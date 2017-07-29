603a222dac334490ca170f61172ed5c2a1a3fec2
YeonTaeKIM <urbanscenery@gmail.com>
2017-07-29 14:08:45 +0900

    로그인단 구현

--- press/routes/index.js
+++ press/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var login = require('./login');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/login', login);
+
+
 
 module.exports = router;
